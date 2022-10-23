46dd417f27cd593422c0e275cf9a102df4cca5d0
dhanubvc <96643681+dhanubvc@users.noreply.github.com>
2022-10-23 22:58:39 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -5,4 +5,8 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+  function Hello(props) {
+  return <h1>Hello World!</h1>;
+}
+
 });
